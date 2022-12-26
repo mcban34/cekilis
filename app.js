48cb5a1a6573e11d9af7548b5af4024a42375a10
mcban34 <ccobanmehmet@gmail.com>
2022-12-26 13:50:58 +0300

    winner list

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
+function copyMessage() {
+  // Mesajınızın değerini alın
+  var message = "mcban,semraerdognn,bayramunl,burakylcn,araseda,mehmetaras34,cobanahmet36,mertaydn,merymcbn,ibrahimgediktr,ardakoskiss,berkayülger,neosyazilim,furkanylmz,omeraydin,tolgabilim";
+
+  // Kopyalama işlemini gerçekleştirin
+  navigator.clipboard.writeText(message).then(function() {
+    console.log("Mesaj panosuna kopyalandı.");
+  }, function(err) {
+    console.error("Kopyalama işlemi başarısız oldu: ", err);
+  });
+  const alert = document.querySelector(".alert");
+  alert.style.display="inline-block";
+  setTimeout(function(){
+    alert.style.display="none";
+  },2000);
+}
+
+document.querySelector(".parentCekilisSonucu").style.display="none";
+
 function tikla(){
 
   let isimler = document.querySelector(".isimler").value;
+  let kazananSayisi = Number(document.querySelector(".kazananSayisi").value);
+  let yedekSayisi = Number(document.querySelector(".yedekSayisi").value);
+
+  if(isimler=="" || kazananSayisi=="" || yedekSayisi==""){
+    return document.querySelector(".alert-danger").style.display="block";
+  }
+
+
+  if(isimler.length<35){
+    document.querySelector(".alert-danger").innerHTML="Çok Az Kullanıcı Adına Sabipsiniz!";
+    document.querySelector(".alert-danger").style.display="block";
+    // alert("Çok Az Kullanıcı Adına Sabipsiniz!");
+    return;
+  }
  
   let bolum =  isimler.split(",");
   
   let isimlerLenght = bolum.length;
 
-  let kazananSayisi = Number(document.querySelector(".kazananSayisi").value);
-  let yedekSayisi = Number(document.querySelector(".yedekSayisi").value);
+  
 
   let kazananlar = [];
   let yedekler = [];
@@ -61,5 +93,30 @@ function tikla(){
   console.log(uniqueArray);
   console.log(uniqueArrayYedek);
 
+
+    // Ul elementi oluşturun
+    const kazanan = document.querySelector(".kazanan");
+  
+    // Dizideki her bir öğe için li elementi oluşturun
+    for (var i = 0; i < uniqueArray.length; i++) {
+      var item = document.createElement("li");
+      item.appendChild(document.createTextNode(uniqueArray[i]));
+      kazanan.appendChild(item);
+    }
+    
+     // Ul elementi oluşturun
+     const yedek = document.querySelector(".yedek");
+  
+     // Dizideki her bir öğe için li elementi oluşturun
+     for (var i = 0; i < uniqueArrayYedek.length; i++) {
+       var item2 = document.createElement("li");
+       item2.appendChild(document.createTextNode(uniqueArrayYedek[i]));
+       yedek.appendChild(item2);
+     }
+
+
+
+    document.querySelector(".parent-content").style.display="none";
+    document.querySelector(".parentCekilisSonucu").style.display="block";
 
 }
